ec2324b827eac5c4a05fa23d749a2a2160f6a2c5
Ivan Karol <ikarol@gomel.iba.by>
2017-04-19 00:02:29 +0300

    alternative model generator

--- config/app.php
+++ config/app.php
@@ -164,6 +164,7 @@ return [
         Illuminate\View\ViewServiceProvider::class,
         Way\Generators\GeneratorsServiceProvider::class,
         Xethron\MigrationsGenerator\MigrationsGeneratorServiceProvider::class,
+        Krlove\EloquentModelGenerator\Provider\GeneratorServiceProvider::class,
 
 
         /*
