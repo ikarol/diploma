984d6219b2cf3d46c7f7e999a81989815f70c95c
Ivan Karol <ikarol@gomel.iba.by>
2017-05-29 21:10:50 +0300

    changed home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,9 +22,7 @@
                             <ul>
                                 <li>@lang('Placement of tasks for implementation, indicating the topic of the project , a brief description of the work, expected development tools')</li>
                                 <li>@lang('Confirmation and rejection of applications of specific students for work execution')</li>
-                                <li>@lang('Setting deadlines and marking the success of the project stages execution')</li>
-                                <li>@lang('Entering a schedule of student consultations and notes on attending consultations')</li>
-                                <li>@lang('Performing preliminary assessment of works')</li>
+                                <li>@lang('Setting deadlines of the project stages execution')</li>
                             </ul>
                         </div>
                     </div>
@@ -37,10 +35,8 @@
                         <div class="panel-body">
                             <ul>
                                 <li>@lang('Viewing a list of available themes')</li>
-                                <li>@lang('Submit your own themes and development tools')</li>
                                 <li>@lang('Applying for the selected theme')</li>
-                                <li>@lang('Notification of the scientific director on the progress of the project')</li>
-                                <li>@lang('Review schedules of consultations and preliminary assessments for the project')</li>
+                                <li>@lang('Review schedules of consultations for the project')</li>
                             </ul>
                         </div>
                     </div>
